11685496d2780a35d8e0d0c8182e011484cd1bd3
Valerii Martell <valerii.martell@gmail.com>
2022-06-20 07:01:45 +0200

    Add the Actor model to the app

--- src/models.py
+++ src/models.py
@@ -37,3 +37,15 @@ class Movie(db.Model):
             'length': self.length,
             'rating': self.rating,
         }
+
+
+class Actor(db.Model):
+    __tablename__ = 'actors'
+
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(50), unique=True, nullable=False)
+    birthday = db.Column(db.Date)
+    is_active = db.Column(db.Boolean, default=False)
+
+    def __repr__(self):
+        return f'Actor({self.name}, {self.birthday})'
